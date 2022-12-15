65c79859877445adb94d1fc85746590651adf252
Ahmed <ahmedsarhan1515@gmail.com>
2022-12-15 16:13:22 +0200

    First commit

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { Counter } from "./features/counter/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <Counter />
+        <p>
+          Edit <code>src/App.tsx</code> and save to reload.
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
