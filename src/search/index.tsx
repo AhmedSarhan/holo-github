a2216b9e73cfcd6fa4b6b68cceb374a4efc3836f
Ahmed <ahmedsarhan1515@gmail.com>
2022-12-16 18:48:51 +0200

    refactor: updating header ui

--- src/search/index.tsx
+++ src/search/index.tsx
@@ -3,17 +3,24 @@ import SearchForm from "./components/form";
 import ReposListing from "./components/repos/repos-listing";
 import { useSearchForm } from "./resources/context";
 import UsersListing from "./components/users/users-listing";
+import styled from "styled-components";
 
 const SearchPage = () => {
   const {
     formValues: { queryType },
   } = useSearchForm();
   return (
-    <div id="search-listing">
+    <Container>
       <SearchForm />
       {queryType === "repo" ? <ReposListing /> : <UsersListing />}
-    </div>
+    </Container>
   );
 };
 
 export default SearchPage;
+
+const Container = styled.div`
+  margin-inline: auto;
+  padding-inline: 1.75rem;
+  margin-top: 30px;
+`;
