55df2a48192d1e25c9aff3a3d5b87d5b79777713
Ahmed <ahmedsarhan1515@gmail.com>
2022-12-16 20:18:44 +0200

    feat: styling-user-card

--- src/search/components/users/user-card.tsx
+++ src/search/components/users/user-card.tsx
@@ -1,16 +1,21 @@
 import React, { FC } from "react";
+import styled from "styled-components";
 import { User } from "../../resources/types";
 
 const UserCard: FC<{ user: User }> = ({ user }) => {
   return (
-    <li>
+    <Card>
       <a href={user.html_url} target="_blank" rel="noreferrer">
         <img src={user.avatar_url} alt={user.login} width="75" height="75" />
         <h6>{user.login}</h6>
       </a>
       <p>{user.type}</p>
-    </li>
+    </Card>
   );
 };
 
 export default UserCard;
+
+const Card = styled.li`
+  box-shadow: 1px 2px 4px 1px rgba(0, 0, 0, 0.4);
+`;
