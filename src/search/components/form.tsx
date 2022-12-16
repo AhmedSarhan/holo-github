e31455803709a4024edb94d115e54a0a87aa7e0b
Ahmed Sarhan <ahmedsarhan1515@gmail.com>
2022-12-16 18:49:37 +0200

    refactor: updating header ui (#3)

--- src/search/components/form.tsx
+++ src/search/components/form.tsx
@@ -1,12 +1,21 @@
+import styled from "styled-components";
+import Logo from "../../app/components/logo";
 import { useSearchForm } from "../resources/context";
 
 const SearchForm = () => {
   const { formValues, updateValue } = useSearchForm();
   const { queryType, searchQuery } = formValues;
   return (
-    <div>
-      <form role="search" onSubmit={(e) => e.preventDefault()}>
-        <input
+    <header>
+      <Hero>
+        <Logo />
+        <div>
+          <h1>Github Searcher</h1>
+          <h3>Search users or repositories below</h3>
+        </div>
+      </Hero>
+      <Form role="search" onSubmit={(e) => e.preventDefault()}>
+        <Input
           aria-label="Search"
           placeholder="Search"
           type="search"
@@ -14,13 +23,64 @@ const SearchForm = () => {
           value={searchQuery}
           onChange={updateValue}
         />
-        <select name="queryType" value={queryType} onChange={updateValue}>
+        <Select name="queryType" value={queryType} onChange={updateValue}>
           <option value="user">User</option>
           <option value="repo">Repository</option>
-        </select>
-      </form>
-    </div>
+        </Select>
+      </Form>
+    </header>
   );
 };
 
 export default SearchForm;
+
+const Hero = styled.div`
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  svg {
+    margin-inline-end: 5px;
+    width: 50px;
+    height: 50px;
+  }
+  div {
+    margin-inline-start: 5px;
+  }
+  h1 {
+    font-size: 24px;
+    font-weight: 900;
+    margin: 0;
+    margin-bottom: 5px;
+  }
+  h3 {
+    color: darkgray;
+    margin: 0;
+  }
+`;
+const Form = styled.form`
+  display: flex;
+  margin-block: 15px;
+  justify-content: flex-start;
+  align-items: center;
+`;
+const inputStyles = `
+  border: 1.5px solid #c4c4c4;
+  border-radius: 2px;
+  outline: none;
+  color: darkgray;
+  
+  &:focus {
+    outline: none;
+  }
+`;
+
+const Input = styled.input`
+  margin-inline-end: 5px;
+  padding: 10px;
+  ${inputStyles};
+`;
+const Select = styled.select`
+  margin-inline-start: 5px;
+  padding: 9px;
+  ${inputStyles};
+`;
